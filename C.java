8bdddf0636a52e5ee9e87b19513795a657902522
WillianBrandao <willianbsufla@gmail.com>
2024-10-24 08:58:17 -0300

    Criado o Método MC3()

--- C.java
+++ C.java
@@ -5,12 +5,16 @@ public class C {
     private int C2;
 
 
-    
+
     public void MC1(){
         System.out.println("MC1");
     }
 
     public void MC2(){
         System.out.println("MC2");
+    }
+
+    public void MC3(){
+        System.out.println("MC3");
     }
 }
